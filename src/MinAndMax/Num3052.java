21139ef53f35174d3992569f66c1fb7059c40bd5
jaelyung <isiskny0304@gmail.com>
2021-05-05 23:52:20 +0900

    다시생각해보기

--- src/MinAndMax/Num3052.java
+++ src/MinAndMax/Num3052.java
@@ -1,9 +1,30 @@
 package MinAndMax;
 
+import java.util.Scanner;
+
 public class Num3052 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int[] input = new int[10];
+		int[] value = new int[input.length];
+		int count = 1;
+		
+		for(int i=0; i<input.length; i++) {
+			input[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<input.length; i++) {
+			value[i] = input[i] % 42;
+			for(int j=i+1; j<value.length; j++) {
+				if(value[j] != value[i]) {
+					count++;
+				}
+			}
+		}
+		
+		System.out.println(count);
 
 	}
 
