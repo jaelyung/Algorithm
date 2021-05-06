8e711218fe4faee285260d532613e29d21407421
jaelyung <isiskny0304@gmail.com>
2021-05-06 23:40:28 +0900

    0506

--- src/MinAndMax/Num3052.java
+++ src/MinAndMax/Num3052.java
@@ -1,31 +1,52 @@
 package MinAndMax;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Num3052 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		Scanner sc = new Scanner(System.in);
 		
-		int[] input = new int[10];
-		int[] value = new int[input.length];
-		int count = 1;
+		//처음에 생각했던 것 
+//		int[] input = new int[10];
+//		int[] value = new int[input.length];
+//		int count = 1;
+//		
+//		for(int i=0; i<input.length; i++) {
+//			input[i] = sc.nextInt();
+//		}
+//		
+//		for(int i=0; i<input.length; i++) {
+//			value[i] = input[i] % 42;
+//			for(int j=i+1; j<value.length; j++) {
+//				if(value[j] != value[i]) {
+//					count++;
+//				}
+//			}
+//		}
+//		
+//		System.out.println(count);
 		
-		for(int i=0; i<input.length; i++) {
-			input[i] = sc.nextInt();
+		//블로그참고
+		
+		boolean[] arr = new boolean[42];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i=0; i<10; i++) {
+			arr[Integer.parseInt(br.readLine()) % 42] = true;
 		}
 		
-		for(int i=0; i<input.length; i++) {
-			value[i] = input[i] % 42;
-			for(int j=i+1; j<value.length; j++) {
-				if(value[j] != value[i]) {
-					count++;
-				}
+		int count = 0;
+		for(boolean value: arr) {
+			if(value) {
+				count++;
 			}
 		}
 		
 		System.out.println(count);
-
 	}
 
 }
