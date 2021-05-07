161f52eaab86838ed293603fe203390a88c7774b
jaelyung <61307199+jaelyung@users.noreply.github.com>
2021-05-07 23:59:46 +0900

    Update Num1546.java

--- src/MinAndMax/Num1546.java
+++ src/MinAndMax/Num1546.java
@@ -1,5 +1,29 @@
+package MinAndMax;
+
+import java.util.*;
+
 public class Num1546{
    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
      
+      int totalExamNum = sc.nextInt();
+      double[] score = new double[totalExamNum];
+      int max = 0;
+      for(int i=0; i<totalExamNum; i++){
+           score[i] = sc.nextInt();
+           max = score[0];
+      }
+      
+      for(int i=1; i<totalExamNum; i++){
+         if(score[i]>max){
+            max=score[i];
+         }
+      }
+      
+      for(int i=0; i<totalExamNum; i++{
+         score[i] = score[i]/max*100
+      }
+          
+      
    }
 }
