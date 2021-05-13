e2793b5f624105f9527ef20c0efc4914c053c549
jaelyung <isiskny0304@gmail.com>
2021-05-13 22:35:29 +0900

    05.13

---
+++ src/makeFunc/Num15596.java
@@ -0,0 +1,14 @@
+package makeFunc;
+
+public class Num15596 {
+	
+	public long sum(int[] a) {
+		long result = 0;
+		
+		for(int i=0; i<a.length; i++) {
+			 result = result + a[i];
+		}
+		
+		return result;
+	}
+}
