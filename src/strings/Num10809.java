135a45a74d6c2b08ed03f8106804a0dbc99ae53a
jaelyung <isiskny0304@gmail.com>
2021-05-16 23:44:11 +0900

    05.16

---
+++ src/strings/Num10809.java
@@ -0,0 +1,25 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Num10809 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		
+		int[] temp = new int[26];
+		
+		for(int j=97; j<=122; j++) {
+			for(int i=0; i<input.length(); i++) {
+				if(j == input.charAt(i)) {
+					
+					
+				}
+			}
+		}
+		
+	}	
+	
+}
