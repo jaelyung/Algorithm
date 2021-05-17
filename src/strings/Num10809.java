62aa5f2899e5e6833d703ed3da6cc2ada00d2b09
jaelyung <isiskny0304@gmail.com>
2021-05-17 23:59:49 +0900

    05.17

--- src/strings/Num10809.java
+++ src/strings/Num10809.java
@@ -18,19 +18,30 @@ public class Num10809 {
 			temp[i] = -1;
 		}
 		
-		//4. 아스키코드 a~z를 의미하는 97~122
-		for(int j=97; j<=122; j++) {
-			//5. 문자열 index (0) 부터 끝까지
-			for(int i=0; i<input.length(); i++) {
-				if(temp[i] == -1 && j == input.charAt(i)) {
-					System.out.println("input.charAt(i)>>>>>>"+ input.charAt(i));
-					//temp[j]가 -1이고 j가 97일때 a가 input.charA(0)과 같다면 
-					//temp[j]에 input의 index값을 넣는다.
-					temp[i]=i;					
-				}
+		for(int i=0; i<input.length(); i++) {
+			char letter = input.charAt(i);
+
+			if(temp[letter - 'a'] == -1) {
+				temp[letter - 'a'] = i;
 			}
 		}
 		
+		
+		
+		
+		//4. 아스키코드 a~z를 의미하는 97~122
+//		for(int j=97; j<=122; j++) {
+//			//5. 문자열 index (0) 부터 끝까지
+//			for(int i=0; i<input.length; i++) {
+//				if(temp[i] == -1 && input[i].charAt(i) == j) {
+//					System.out.println("input.charAt(i)>>>>>>"+ input[i].charAt(i));
+//					//temp[j]가 -1이고 j가 97일때 a가 input.charA(0)과 같다면 
+//					//temp[j]에 input의 index값을 넣는다.
+//					temp[i]=i;					
+//				}
+//			}
+//		}
+			
 		for(int i=0; i<26; i++) {
 			System.out.print(temp[i] + " ");
 		}
