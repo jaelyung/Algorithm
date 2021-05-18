ca808a38e2537e42731dd344bbcd5eabc6459209
jaelyung <isiskny0304@gmail.com>
2021-05-18 23:46:28 +0900

    05.18

---
+++ src/strings/Num2675.java
@@ -0,0 +1,30 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Num2675 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = Integer.parseInt(sc.nextLine());
+		String[] input = new String[testCase];
+		
+		for(int i=0; i<testCase; i++) {
+			input[i] = sc.nextLine();
+			
+		}
+		
+//		for(int i=0; i<testCase; i++) {
+//			String[] temp = input[i].split(" ");
+//			//temp[0]=3, temp[1]=ABC
+//			for(int j=0; j<Integer.parseInt(temp[0]); j++) {
+//				System.out.print(temp[1].charAt(j));
+//				System.out.print(temp[1].charAt(j));
+//				System.out.print(temp[1].charAt(j));
+//			}
+//		}
+
+	}
+
+}
