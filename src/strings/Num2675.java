faa111bec97fc05f13baf68df1197a2ecaf1be6b
jaelyung <isiskny0304@gmail.com>
2021-05-19 23:01:30 +0900

    05.19

--- src/strings/Num2675.java
+++ src/strings/Num2675.java
@@ -8,22 +8,19 @@ public class Num2675 {
 		Scanner sc = new Scanner(System.in);
 		
 		int testCase = Integer.parseInt(sc.nextLine());
-		String[] input = new String[testCase];
 		
-		for(int i=0; i<testCase; i++) {
-			input[i] = sc.nextLine();
+		for(int i=0; i<testCase; i++){ 				
 			
+			int num = sc.nextInt();
+			String str = sc.next();
+			
+			for(int j=0; j<str.length(); j++){ 
+				for(int k=0; k<num; k++){		
+					System.out.print(str.charAt(j));
+				}
+			}
 		}
-		
-//		for(int i=0; i<testCase; i++) {
-//			String[] temp = input[i].split(" ");
-//			//temp[0]=3, temp[1]=ABC
-//			for(int j=0; j<Integer.parseInt(temp[0]); j++) {
-//				System.out.print(temp[1].charAt(j));
-//				System.out.print(temp[1].charAt(j));
-//				System.out.print(temp[1].charAt(j));
-//			}
-//		}
+		System.out.println();
 
 	}
 
