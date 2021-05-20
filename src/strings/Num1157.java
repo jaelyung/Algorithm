f5d4ebb1c0b7f5db8c296b0ef89415dd532915a1
jaelyung <isiskny0304@gmail.com>
2021-05-20 23:44:41 +0900

    05.20

--- src/strings/Num1157.java
+++ src/strings/Num1157.java
@@ -1,10 +1,15 @@
 package strings;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Num1157 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		//规过 1. 皋葛府 檬苞
+		/*
 		Scanner sc = new Scanner(System.in);
 		
 		String input = sc.nextLine();
@@ -38,6 +43,36 @@ public class Num1157 {
 		}
 		
 		System.out.println(maxLetter);
+		*/
+		
+		//规过2
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[26];
+		String s = br.readLine();
+		
+		for(int i=0; i<s.length(); i++) {
+			if('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
+				arr[s.charAt(i)-97]++;
+			} else {
+				arr[s.charAt(i)-65]++;
+			}
+		}
+		
+		int max = -1;
+		char ch = '?';
+		for(int i=0; i<26; i++) {
+			
+			if(arr[i] > max) {
+				max = arr[i];
+				ch = (char) (i+65);
+			}
+			else if(arr[i] == max) {
+				ch = '?';
+			}
+		}
+		System.out.print(ch);
 		
 	}
 
