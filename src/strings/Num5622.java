33bcadd9e4ed1a3fa2d0c02a623cfc61ec5a81c5
jaelyung <isiskny0304@gmail.com>
2021-05-25 23:53:49 +0900

    05.25

--- src/strings/Num5622.java
+++ src/strings/Num5622.java
@@ -1,9 +1,26 @@
 package strings;
 
+import java.util.Scanner;
+
 public class Num5622 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		String[] temp = {"","","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+		String[] charInput = input.split("");
+		int count = 0;
+		
+		for(int i=0; i<charInput.length; i++) {
+			for(int j=0; j<temp.length; j++) {
+				if(temp[j].contains(charInput[i])) {
+					count = count + (j+1);
+				}
+			}
+		}
+		
+		System.out.println(count);
 
 	}
 
