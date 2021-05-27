3b56dde0f74dda609daa735be402b02745d3eba6
jaelyung <isiskny0304@gmail.com>
2021-05-27 23:10:02 +0900

    05.27

---
+++ src/strings/Num1316.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Num1316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = Integer.parseInt(sc.nextLine());
+		String[] input = new String[num];
+		for(int i=0; i<num; i++) {
+			input[i] = sc.nextLine();
+			for(int j=0; j<input[i].length(); j++) {
+				
+			}
+		}
+
+	}
+
+}
