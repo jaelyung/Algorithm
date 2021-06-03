9d651ba88867aa133324d862b04c7e4e6120e38b
jaelyung <isiskny0304@gmail.com>
2021-06-03 23:13:08 +0900

    06.03

---
+++ src/math1/Num1193.java
@@ -0,0 +1,20 @@
+package math1;
+
+import java.util.Scanner;
+
+public class Num1193 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		/*
+		 * 피라미드
+		 * 홀수번 똑바로 출력, 짝수번 뒤집어서 출력
+		 * 
+		 */
+
+	}
+
+}
