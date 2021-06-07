88050023489e9482bff3cf9f0c2d29ed291d1cea
jaelyung <isiskny0304@gmail.com>
2021-06-07 23:37:01 +0900

    06.07

--- src/math1/Num1193.java
+++ src/math1/Num1193.java
@@ -7,14 +7,24 @@ public class Num1193 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int input = sc.nextInt();
-		
-		/*
-		 * 피라미드
-		 * 홀수번 똑바로 출력, 짝수번 뒤집어서 출력
-		 * 
-		 */
+		int n = sc.nextInt();
 
+		int cross_count = 1, prev_count_sum = 0;
+		
+		while(true) {
+			
+			if(n <= prev_count_sum + cross_count) {
+				
+				if(cross_count % 2 == 1) {
+					System.out.println((cross_count - (n - prev_count_sum -1)) + "/");
+				}
+			}
+		}
+		
+		
+
+		
+		
 	}
 
 }
