88f83530ce0cade46e2e133410c9b3ebfddc7e41
jaelyung <isiskny0304@gmail.com>
2021-06-10 23:26:05 +0900

    06.10

---
+++ src/math1/Num2869.java
@@ -0,0 +1,14 @@
+package math1;
+
+import java.util.Scanner;
+
+public class Num2869 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int v = Integer.parseInt(sc.nextLine());
+
+	}
+
+}
